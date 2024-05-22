14eb70daee5f7f3805c199513be7f2f76e36318e
ryanhirth <64804689+ryanhirth@users.noreply.github.com>
2024-05-22 10:08:07 -0400

    separate files for organization

---
+++ cruise-app/src/main/scala/bestGroupPrice.scala
@@ -0,0 +1,31 @@
+def getBestGroupPrices(rates: Seq[Rate],
+                       prices: Seq[CabinPrice]): Seq[BestGroupPrice] = {
+
+  // 1 - converting the list of cabin prices to a list of group prices
+  var groupPrices = prices.map(cabinPrice => BestGroupPrice(cabinPrice.cabinCode, cabinPrice.rateCode, cabinPrice.price, rates.find(rate => rate.rateCode == cabinPrice.rateCode).get.rateGroup))
+
+  // 2 - group the group prices by cabin code and rate group for lowest price
+  var bestGroupPrices = groupPrices
+    .groupBy(groupPrice => (groupPrice.cabinCode, groupPrice.rateGroup))
+    .map { case (grouping, priceGroupObjects) => priceGroupObjects.minBy(groupPrice => groupPrice.price) }
+
+  // 3 - keep original ordering - return group prices in the bestGroupPrices list
+  return groupPrices.filter(groupPrice =>
+    bestGroupPrices.exists(best =>
+      best.cabinCode == groupPrice.cabinCode
+        && best.rateCode == groupPrice.rateCode
+        && best.price == groupPrice.price))
+}
+case class Rate(rateCode: String, rateGroup: String)
+case class CabinPrice(cabinCode: String,
+                      rateCode: String,
+                      price: BigDecimal)
+case class BestGroupPrice(cabinCode: String,
+                          rateCode: String,
+                          price: BigDecimal,
+                          rateGroup: String) {
+  override def toString: String = {
+    val priceFormatted = f"$price%.2f"
+    s"BestGroupPrice($cabinCode, $rateCode, $priceFormatted, $rateGroup)"
+  }
+}
