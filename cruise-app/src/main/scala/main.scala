c3dec657040a47f86877757c1b903bae49b8f038
ryanhirth <64804689+ryanhirth@users.noreply.github.com>
2024-05-22 07:20:55 -0400

    wip

--- cruise-app/src/main/scala/main.scala
+++ cruise-app/src/main/scala/main.scala
@@ -1,4 +1,45 @@
+val M1: String = "M1"
+val M2: String = "M2"
+val Military: String = "Military"
+val S1: String = "S1"
+val S2: String = "S2"
+val Senior: String = "Senior"
+val CA: String = "CA"
+val CB: String = "CB"
+
 @main
 def main(): Unit = {
-  println("Hello world!")
+  println("Hello world, first to find the best price for each rate group!")
+  var rates = Seq(
+    Rate(M1, Military),     
+    Rate(M2, Military),
+    Rate(S1, Senior), 
+    Rate(S2, Senior)
+  )
+  var cabinPrices = Seq(
+    CabinPrice(CA, M1, 200.00),
+    CabinPrice(CA, S1, 225.00),
+    CabinPrice(CA, M2, 250.00),
+    CabinPrice(CA, S2, 260.00),
+    CabinPrice(CB, M1, 230.00),
+    CabinPrice(CB, M2, 260.00),
+    CabinPrice(CB, S1, 245.00),
+    CabinPrice(CB, S2, 270.00)
+  )
+  var result = getBestGroupPrices(rates, cabinPrices)
+  println(result)
 }
+
+def getBestGroupPrices(rates: Seq[Rate],
+                       prices: Seq[CabinPrice]): Seq[BestGroupPrice] = {
+  
+return Seq()
+}
+case class Rate(rateCode: String, rateGroup: String)
+case class CabinPrice(cabinCode: String,
+                      rateCode: String,
+                      price: BigDecimal)
+case class BestGroupPrice(cabinCode: String,
+                          rateCode: String,
+                          price: BigDecimal,
+                          rateGroup: String)
